cd117bc7b61c3e50019bd1606dbbf62b27d66daa
sankar <sankusanku001@gmail.com>
2024-10-05 13:38:56 +0100

    db design completed

--- ecom/settings.py
+++ ecom/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+    'customers',
+    'orders',
+    'themes',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +78,13 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecom',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
     }
 }
 
--- orders/models.py
+++ orders/models.py
@@ -18,12 +18,12 @@ class Order(models.Model):
                      (ORDER_REJECTED, 'ORDER_REJECTED'))
 
     order_status = models.IntegerField(choices=STATUS_CHOICE, default=CART_STAGE)
-    owner = models.ForeignKey(Customer, on_delete=models.SET_NULL, related_name='orders')
+    owner = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, related_name='orders')
     delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class OrderedItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, related_name='added_carts')
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name='added_carts')
     quantity = models.IntegerField(default=1)
     owner = models.ForeignKey(Order, related_name='added_items', on_delete=models.CASCADE)
